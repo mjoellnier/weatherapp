92fe6cf3b27f4274ffab3a7819b842081e0c9139
mjoellnier <other@max-wuest.de>
2019-08-20 20:31:09 +0200

    Added usePosition hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./css/weather-icons.min.css";
+import axios from "axios";
+import { usePosition } from "./components/geolocation";
 
 function App() {
+  const [weatherApiResponse, setWeatherApiResponse] = useState({
+    name: "...loading"
+  });
+  const [firstTime, setFirstTime] = useState(true);
+  const appId = "8f1e29cb0f7a12f19c6d87da31359214";
+  let { latitude, longitude, error } = usePosition();
+
+  /**
+   * Notice that there is a single useEffect hook which can replace both componentDidMount and
+   * componentDidUpdate. To run the hook only once we can use the second argument to useEffect
+   * — an array of values that the effect depends on. By default, the effect will run when any
+   * of the props or state changes. If we pass an empty array, the effect will only run on
+   * first render.
+   */
+  useEffect(() => {
+    if (firstTime && latitude && longitude) {
+      setFirstTime(false);
+      const path =
+        "http://api.openweathermap.org/data/2.5/weather?lat=" +
+        latitude +
+        "&lon=" +
+        longitude +
+        "&appid=" +
+        appId;
+      axios.get(path).then(({ data }) => {
+        setWeatherApiResponse(data);
+      });
+    }
+  }, [latitude, longitude]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <div id="weatherBox">
+        <h1>The weather in {weatherApiResponse.name}</h1>
+        <p>The weather will be good :)</p>
+        <i id="weatherIcon" class="wi wi-na" />
+      </div>
     </div>
   );
 }
